02c088e181caaa7b8388acdc1e5cd4b5ed7be113
thdtjsdn <thdtjsdn@gmail.com>
2018-11-29 15:11:03 +0900

    [ root ] - N - Mac용 커맨드 파일 추가

---
+++ root/bin_mac/ui--createJSON__Locale__CheckOriginalJSON.js
@@ -0,0 +1,33 @@
+//----------------------------------------------------------------------------------------------------;
+var fileNm = "ui--createJSON__Locale.js";
+if( console ) console.log( "[ S ] - " + fileNm + "----------" );
+//----------------------------------------------------------------------------------------------------;
+
+require( "./_$TATIC_CONST_TtwApplicationCommon.js" );
+
+//----------------------------------------------------------------------------------------------------;
+
+//*/
+var rootPath = "../ui/";
+/*/
+var rootPath = "../ui_template/";
+//*/
+
+var extension = ".html";
+
+//Overwrite true시 다른 JSON들도 전부 초기화 되니 주의;
+var bOverWrite = false;
+
+//지정 폴더;
+SUtilFsWriteStream_Extension_Dev.create_TemplateJSON_FromHTMLDirectory__CheckOriginalJSON( rootPath, extension, bOverWrite );
+
+//하위폴더 모두 포함;
+SUtilFsWriteStream_Extension_Dev.create_TemplateJSON_FromHTMLDirectory__SubDirectories__CheckOriginalJSON( rootPath, extension, bOverWrite );
+
+//----------------------------------------------------------------------------------------------------;
+
+global.process.exit();
+
+//----------------------------------------------------------------------------------------------------;
+if( console ) console.log( "[ E ] - " + fileNm + "----------" );
+//----------------------------------------------------------------------------------------------------;
